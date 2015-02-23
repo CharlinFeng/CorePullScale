2dab6596addb32c114a6f8d0a6dd997a8d316dea
沐汐 <muxi@muxideiMac.local>
2015-02-23 11:16:57 +0800

    Initial Commit

---
+++ CorePullScale/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CorePullScale
+//
+//  Created by 沐汐 on 15-2-23.
+//  Copyright (c) 2015年 沐汐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
